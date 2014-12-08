d04880f9da17afddd4eb5544448a9c06387640cd
ohNegative <itsinthewiki@gmail.com>
2014-12-08 16:01:15 -0600

    Update invoke.js
    small updates to clean up the if/then statement  Pete

--- public/js/lib/invoke.js
+++ public/js/lib/invoke.js
@@ -10,13 +10,13 @@ function headerImage() {
     if (document.location.pathname == '/residential') {
         $aia('header#head-internal').css('background-image', 'url(/images/residential.jpg)');
     }
-    if (document.location.pathname == '/commercial') {
+    else if (document.location.pathname == '/commercial') {
         $aia('header#head-internal').css('background-image', 'url(/images/commercial.jpg)');
     }
-    if (document.location.pathname == '/work') {
+    else if (document.location.pathname == '/work') {
         $aia('header#head-internal').css('background-image', 'url(/images/work.jpg)');
     }
-    if (document.location.pathname == '/contact') {
+    else if (document.location.pathname == '/contact') {
         $aia('header#head-internal').css('background-image', 'url(/images/haunted-o.jpg)');
     }
 }
